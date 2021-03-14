765edb4dc4693c3a0af48d51af86265b3ef7f8d9
FakeModz <78726008+FakeModz@users.noreply.github.com>
2021-03-14 11:10:01 +0700

    Update events.cpp

--- proxy/events.cpp
+++ proxy/events.cpp
@@ -222,12 +222,12 @@ bool events::out::generictext(std::string packet) {
             for (auto& player : g_server->m_world.players) {
                 auto name_2 = player.name.substr(2); //remove color
                 if (name_2.find(username)) {
-                  g_server->send(false, "action|input\n|text|/msg /" + username + "action|input\n|text|`4Message From FakeModz YT `$: TEST);
+                  g_server->send(false, "action|input\n|text|/msg /" + username + "action|input\n|text|`4Message From FakeModz YT `$: TEST");
                    // g_server->send(false, "action|wrench\n|netid|" + std::to_string(player.netid));
                    // std::this_thread::sleep_for(std::chrono::milliseconds(5));
                    // g_server->send(false, "action|dialog_return\ndialog_name|popup\nnetID|" + std::to_string(player.netid) + "|\nbuttonClicked|worldban"); 
                     // You Can |kick |trade |worldban 
-                    std::this_thread::sleep_for(std::chrono::milliseconds(5));
+                   // std::this_thread::sleep_for(std::chrono::milliseconds(5));
                     gt::send_log("`4Message all people in world");
                   
                 }
